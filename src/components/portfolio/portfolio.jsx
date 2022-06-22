d541780ae1bc777bbcc0ee3833b601164f1571aa
Aswin K <aswinkarat@gmail.com>
2022-06-22 23:38:08 +0530

    added portfolio jsx

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
 import './portfolio.css'
 
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
 const Portfolio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+      <h5>Recent Work</h5>
+      <h2>Portfolio</h2>
+      
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="http://github.com" className='btn' rel='noreferrer' target='_blank'>GitHub</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' rel='noreferrer' target='_blank'>Live Demo</a>
+        </article>
+        
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="http://github.com" className='btn' rel='noreferrer' target='_blank'>GitHub</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' rel='noreferrer' target='_blank'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="http://github.com" className='btn' rel='noreferrer' target='_blank'>GitHub</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' rel='noreferrer' target='_blank'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="http://github.com" className='btn' rel='noreferrer' target='_blank'>GitHub</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' rel='noreferrer' target='_blank'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="http://github.com" className='btn' rel='noreferrer' target='_blank'>GitHub</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' rel='noreferrer' target='_blank'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="http://github.com" className='btn' rel='noreferrer' target='_blank'>GitHub</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' rel='noreferrer' target='_blank'>Live Demo</a>
+        </article>
+
+      </div>
+    </section>
   )
 }
 
