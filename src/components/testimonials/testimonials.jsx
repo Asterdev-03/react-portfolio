661ffbc0cbd5842666a0ce8aaa8d93d836365f5f
Aswin K <aswinkarat@gmail.com>
2022-06-25 13:26:35 +0530

    added testimonials jsx

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './testimonials.css'
 
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
 const Testimonials = () => {
   return (
-    <section id='testimonials'>testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />            
+          </div>
+          <div className="client__review">
+            <h5 className='client__name'>Ernest Achiever</h5>
+            <small className='client__review'>Good</small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />            
+          </div>
+          <div className="client__review">
+            <h5 className='client__name'>Ernest Achiever</h5>
+            <small className='client__review'>Good</small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />            
+          </div>
+          <div className="client__review">
+            <h5 className='client__name'>Ernest Achiever</h5>
+            <small className='client__review'>Good</small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
