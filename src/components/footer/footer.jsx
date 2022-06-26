8bec7dc4d0216371716715b9a5027daeb60a231a
Aswin K <aswinkarat@gmail.com>
2022-06-26 10:48:40 +0530

    added footer jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './footer.css'
 
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
+
 const Footer = () => {
   return (
-    <section id='footer'>footer</section>
+    <footer>
+      <a href="/#" className='footer__logo'>FOOTER</a>
+
+      <ul className='permalinks'>
+        <li><a href="/#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="http://facebook.com"><FaFacebookF /></a>
+        <a href="http://instagram.com"><FiInstagram /></a>
+        <a href="http://twitter.com"><IoLogoTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; AsterDev. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
