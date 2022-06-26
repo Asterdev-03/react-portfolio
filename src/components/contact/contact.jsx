867692cf3e77a4cb3a2d19766f8e3fb96051cf5e
Aswin K <aswinkarat@gmail.com>
2022-06-26 11:12:56 +0530

    Revert "added form submission using emailjs"
    This reverts commit 049e6c6fc0c9b02c3a8c392bf588d56391b0c645.

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,29 +1,11 @@
-import React, { useRef } from 'react';
+import React from 'react'
 import './contact.css'
 
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerFill} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
 
-import emailjs from 'emailjs-com'
-
 const Contact = () => {
-
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_4oc7rvi', 'template_2u0kt2e', form.current, 'iylweqs4jl38gHqqQ')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-
-    e.target.reset();
-  };
-
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -32,28 +14,28 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail className='contact__option-icon' />
+            <MdOutlineEmail />
             <h4>Email</h4>
             <h5>askme@gmail.com</h5>
             <a href="mailto:askme@gmail.com">Send a message</a>
           </article>
 
           <article className="contact__option">
-            <RiMessengerFill className='contact__option-icon' />
+            <RiMessengerFill />
             <h4>Messenger</h4>
             <h5>hellotutorials</h5>
             <a href="http://m.me/earnest.achiever" rel='noreferrer' target='_blank'>Send a message</a>
           </article>
 
           <article className="contact__option">
-            <BsWhatsapp className='contact__option-icon' />
+            <BsWhatsapp />
             <h4>Whatsapp</h4>
             <h5>+123456789</h5>
             <a href="http://api.whatsapp.com/send?phone+123456789" rel='noreferrer' target='_blank'>Send a message</a>
           </article>
         </div>
 
-        <form ref={form} onSubmit={sendEmail}>
+        <form action="">
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Email' required />
           <textarea name="message" rows="17" placeholder='Your Message' required></textarea>
